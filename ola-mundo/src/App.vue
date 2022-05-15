6daf9df773500e3371a27839006eb321b25af734
Lucas Gabriel Devigili <lucasdevigili@hotmail.com>
2022-05-15 00:06:51 -0300

    Ouvindo a troca de tema

--- ola-mundo/src/App.vue
+++ ola-mundo/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <main class="columns is-gapless is-multiline modo-escuro">
+  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo }">
     <div class="column is-one-quarter">
-      <BarraLateral />
+      <BarraLateral @ao-tema-alterado="trocarTema"/>
     </div>
     <div class="column is-three-quarter conteudo conteudo">
       <Formulario @aoSalvarTarefa="salvarTarefa"/>
@@ -32,7 +32,8 @@ export default defineComponent({
   },
   data () {
     return {
-      tarefas: [] as ITarefa[]
+      tarefas: [] as ITarefa[],
+      modoEscuroAtivo: false
     }
   },
   computed: {
@@ -43,6 +44,9 @@ export default defineComponent({
   methods: {
     salvarTarefa (tarefa: ITarefa) {
       this.tarefas.push(tarefa)
+    },
+    trocarTema (modoEscuroAtivo: boolean) {
+      this.modoEscuroAtivo = modoEscuroAtivo
     }
   }
 });
